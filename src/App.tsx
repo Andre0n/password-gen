a59d827669a5deaaf93f8bd0d996606937f205ef
Andre0n <andreon098@gmail.com>
2023-06-04 15:07:47 -0300

    feat: setup basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { ArrowSquareUpRight } from '@phosphor-icons/react';
+
 export default function App() {
-  return <h1 className="text-sm font-bold"> Hello, World! </h1>;
+  return (
+    <main className="flex h-screen flex-col items-center justify-between bg-gray-900 p-4">
+      <footer>
+        <span className="text-sm text-white">
+          Created with ❤️ by{' '}
+          <a
+            className="inline-flex font-bold"
+            target="_blank"
+            href="https://github.com/Andre0n"
+          >
+            André Gabriel <ArrowSquareUpRight size={22} color="#FAFAFA" />
+          </a>
+        </span>
+      </footer>
+    </main>
+  );
 }
